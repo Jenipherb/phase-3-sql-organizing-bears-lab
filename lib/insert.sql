21b43d3d6b579d59c2bfed8ff15f029f6b061491
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-08-15 09:40:01 -0600

    commit tree 8a8f2f920dcf4429cab1d002755bb4ead22ee503

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 2, "M", "brown", "cuddly", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "black", "spicy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 1, "F", "brown", "sus", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 4, "M", "white", "sus", 1);
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "white", "cuddly", 0);
+INSERT INTO bears VALUES (6, "Grinch", 15, "M", "green", "sour", 1);
+INSERT INTO bears VALUES (7, "Wendy", 3, "F", "brown", "cuddly", 0);
+INSERT INTO bears VALUES (8, null, 1, "M", "black", "cuddly", 0);
+
